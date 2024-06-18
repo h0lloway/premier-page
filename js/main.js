1d3dd900f4b428168afc67f06a861092af487edc
Dubrovin Maksim <65232972+h0lloway@users.noreply.github.com>
2024-06-18 12:56:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,16 @@
 // стрелка якорь
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-  animationTime = 500,
-  framesCount = 60;
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
 
-anchors.forEach(function (item) {
-  item.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-
-    let scroller = setInterval(function () {
-      let scrollBy = coordY / framesCount;
-
-      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        window.scrollBy(0, scrollBy);
-      } else {
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    }, animationTime / framesCount);
-  });
-});
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
 
 
 // модальное окно
